97e6eca479972dde8fee5d393d34c2806516a3c5
EkaterinaYa8 <bkates@mail.ru>
2019-11-30 21:40:40 +0300

    conftest.py fixed

--- conftest.py
+++ conftest.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
 import pytest
-import re
 
 
 @pytest.fixture
-def link(request):
+def languages_initial():
+    languages_initial = ['ar', 'ca', 'cs', 'da', 'de', 'en-gb', 'el', 'es', 'fi', 'fr', 'it', 'ko', 'nl', 'pl', 'pt',
+                         'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans']
+    return languages_initial
+
+
+@pytest.fixture
+def link(request, languages_initial):
     link = f'http://selenium1py.pythonanywhere.com/{request.config.getoption("--language")}/'
+
+    language_from_link = request.config.getoption("--language")
+    if language_from_link == 'zh-cn':
+        language_from_link = ['zh-hans']
+    assert language_from_link in languages_initial, f'Переданное значение языка в аргументах командной строки ' \
+                                                    f'--language={request.config.getoption("--language")} ' \
+                                                    f'некорректно.\nДолжно быть использовано значение из следующего ' \
+                                                    f'списка:\n{languages_initial} или \"zh-cn\".'
     return link
 
 
@@ -18,37 +32,11 @@ def browser():
 
 
 @pytest.fixture
-def languages_initial():
-    languages_initial = ['ar', 'ca', 'cs', 'da', 'de', 'en-gb', 'el', 'es', 'fi', 'fr', 'it', 'ko', 'nl', 'pl', 'pt',
-                         'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans']
-    return languages_initial
-
-
-@pytest.fixture
-def current_language(link, languages_initial):
-
-    if re.findall('\.com/(\w+\-\w+)', link):
-        language_list_from_link = re.findall('\.com/(\w+\-\w+)', link)
-        if language_list_from_link == ['zh-cn']:
-            language_list_from_link = ['zh-hans']
-    else:
-        language_list_from_link = re.findall('\.com/(\w+)', link)
-
-    assert len(language_list_from_link) == 1, \
-        'Ошибка парсинга link при определении значения <language>, ожидается link в формате:\n' \
-        'http://selenium1py.pythonanywhere.com/<language>/\nгде <language> - это элемент списка {}.\n' \
-        'Проверьте link.'.format(languages_initial)
-
-    if language_list_from_link[0] in languages_initial:
-        current_language = language_list_from_link[0]
-
-        return current_language
-
-    else:
-        print('{} отсутствует в списке ожидаемых языков. Удостоверьтесь, что значение языка в конце link корректно.'
-              ''.format(language_list_from_link[0]))
+def current_language(request):
+    current_language = request.config.getoption("--language")
+    return current_language
 
 
 def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default='ru',
-                     help="Выбрать язык для загрузки сайта из списка значений:\n")
+    parser.addoption('--language', action='store', default='ru',
+                     help='Выбрать язык для загрузки сайта из списка значений:\n')
