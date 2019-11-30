357e51166f05dcb8da6a59813023c1ddc1e037bb
EkaterinaYa8 <bkates@mail.ru>
2019-11-30 21:54:04 +0300

    conftest.py fixed

--- conftest.py
+++ conftest.py
@@ -15,12 +15,15 @@ def link(request, languages_initial):
 
     language_from_link = request.config.getoption("--language")
     if language_from_link == 'zh-cn':
-        language_from_link = ['zh-hans']
-    assert language_from_link in languages_initial, f'Переданное значение языка в аргументах командной строки ' \
-                                                    f'--language={request.config.getoption("--language")} ' \
-                                                    f'некорректно.\nДолжно быть использовано значение из следующего ' \
-                                                    f'списка:\n{languages_initial} или \"zh-cn\".'
-    return link
+        return link
+    elif language_from_link == 'zh-hans':
+        return 'http://selenium1py.pythonanywhere.com/zh-cn/'
+    else:
+        assert language_from_link in languages_initial, f'Переданное значение языка в аргументах командной строки ' \
+                                                        f'--language={request.config.getoption("--language")} ' \
+                                                        f'некорректно.\nДолжно быть использовано значение из следующего ' \
+                                                        f'списка:\n{languages_initial} или \"zh-cn\".'
+        return link
 
 
 @pytest.fixture
